47c93937a2f244b9df139305106550a5ece61e9f
Praded3 <o.pradidenko@gmail.com>
2023-06-27 10:51:50 +0300

    done

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -4,11 +4,14 @@ import { Contacts } from './Contacts/Contacts';
 import { TechSkillsAll } from './TechSkillsAll/TechSkillsAll';
 import{ SoftSkils } from '../SideBar/TechSkillsAll/SoftSkills/SoftSkils'
 import skills from '../../Assets/skills.json';
+// import skills from '../../logo.svg';
+
+const logo = require('../../photo.jpg')
 
 const SideBar = () => {
     return (
         <Container>
-            <Photo src="./img/photo.jpg" alt="my photo" width="370"/>
+            <Photo src={logo} alt="my photo" width="570"/>
             <Contacts />
             <TechSkillsAll skills={skills} />
             <SoftSkils/>
--- src/Components/SideBar/SideBar.slyled.js
+++ src/Components/SideBar/SideBar.slyled.js
@@ -3,8 +3,13 @@ import styled from '@emotion/styled';
 export const Container = styled.aside`
   padding-bottom: 44px;
   background-color: #e0ebfe;
+  padding-right: 50px;
 `
 export const Photo = styled.img`
-    height: 370px;
-    margin-top: 50px; 
+ display: flex;
+  height: 370px;
+  width: 500px;
+  margin-top: 50px; 
+  padding-left: 25px;
+  object-fit: cover ;
 `
